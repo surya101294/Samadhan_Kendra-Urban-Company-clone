3d725e7e245c1fda4cb0a6b0d5a2a024c475590f
surya101294 <surya101294@gmail.com>
2022-12-14 16:54:39 +0530

    test

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from "./Components/Navbar"
+import AllRoutes from './Components/AllRoutes';
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <AllRoutes/>
+    </div>
+  );
+}
+
+export default App;
